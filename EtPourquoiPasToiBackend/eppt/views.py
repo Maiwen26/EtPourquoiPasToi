9d9e9181081f78024c413a819b6a8df95e9e7ddb
Maiwen26 <mmaginet@ensc.fr>
2022-03-02 15:22:47 +0100

    tentative de création de form pour les témoignages

--- EtPourquoiPasToiBackend/eppt/views.py
+++ EtPourquoiPasToiBackend/eppt/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Temoignage
 
 # Create your views here.
+def temoignages_list(request):
+    temoignages = Temoignage.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'eppt/temoignages_list.html', {'temoignages':temoignages})
